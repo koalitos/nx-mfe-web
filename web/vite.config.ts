b223de4f81a3746c34f96109f62e32c7fb7e67d3
Lucas Amaral <koalasoftbr@gmail.com>
2025-11-12 11:31:02 -0300

    vit config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,34 +4,38 @@ import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
-export default defineConfig(() => ({
-  root: __dirname,
-  cacheDir: '../node_modules/.vite/web',
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-  preview: {
-    port: 4200,
-    host: 'localhost',
-  },
-  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-  build: {
-    outDir: '../dist/web',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
+export default defineConfig(() => {
+  const previewPort = Number(process.env.PORT ?? 4173);
+
+  return {
+    root: __dirname,
+    cacheDir: '../node_modules/.vite/web',
+    server: {
+      port: 4200,
+      host: '0.0.0.0',
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
-    css: true,
-  },
-}));
+    preview: {
+      port: previewPort,
+      host: '0.0.0.0',
+    },
+    plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+    build: {
+      outDir: '../dist/web',
+      emptyOutDir: true,
+      reportCompressedSize: true,
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['./src/test/setup.ts'],
+      css: true,
+    },
+  };
+});
